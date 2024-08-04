c620266708a7d5b88a33193ac6979df7c102c8cf
unknown <l.kinuthia@alustudent.com>
2024-08-04 13:33:14 +0400

    initial commit

---
+++ build/precache-manifest.9fc1699dca7dab901cc47b4e58fcae9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6e80cd4ef746584d7152a126ceb670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d6a857f26aed4f2c07",
+    "url": "/static/css/main.1eb2f676.chunk.css"
+  },
+  {
+    "revision": "957db35b43b5e4395c58",
+    "url": "/static/js/2.2ccd7710.chunk.js"
+  },
+  {
+    "revision": "75d6a857f26aed4f2c07",
+    "url": "/static/js/main.2aabccf1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
